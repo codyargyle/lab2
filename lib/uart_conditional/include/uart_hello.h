e934b822ac699ab5ee0e0a1ad998d451403f5549
Misael812 <misaelnava812@gmail.com>
2023-10-01 19:56:26 -0600

    Update uart_hello.h
    Testing to see if changes affect build

--- lib/uart_conditional/include/uart_hello.h
+++ lib/uart_conditional/include/uart_hello.h
@@ -1,10 +1,10 @@
 #pragma once
 #ifndef TESTING_ENV
-#include <drivers/uart.h>
-#else
 int test_uart_out(unsigned char byte);
 int test_uart_in(unsigned char* byte);
 struct device{};
+#else
+#include <drivers/uart.h>
 #endif
 
 
